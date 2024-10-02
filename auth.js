58e8655fe9a4f5d42d5195e8a1f8cfa7bb049890
Gurvinder Singh aka Gxuri <66419304+Gurvinder-Singh02@users.noreply.github.com>
2024-10-02 22:12:07 +0530

    initial 🚀

---
+++ auth.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "s3cret";
+
+function auth(req, res, next) {
+
+    const token = req.headers.token;
+
+    if(!token){
+        res.status(403).json({
+            message: "no Token"
+        })
+    }
+
+    const response = jwt.verify(token, JWT_SECRET);
+
+    if (response) {
+        req.userId = response.id;
+        next();
+    } else {
+        res.status(403).json({
+            message: "Incorrect creds"
+        })
+    }
+}
+
+module.exports = {
+    auth,
+    JWT_SECRET
+}
